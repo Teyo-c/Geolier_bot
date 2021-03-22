d5250ad2328c058df5215b04b8f55ceddaa1e193
Théo <80926559+Teyo-c@users.noreply.github.com>
2021-03-22 17:19:30 +0100

    Reformatage commentaires + Ajout de contraintes/fonctionnalités

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import discord
 import os
 import random
 import asyncio
+import nacl
 from discord.ext import commands
 from keep_alive import keep_alive
 
@@ -12,12 +13,12 @@ client = commands.Bot(command_prefix='!')
 async def on_ready():
     await client.change_presence(activity=discord.Game(
         name='Prend son service...'))
-    print('We have logged in as {0.user}'.format(client))
+    print('Nous sommes arrivé à la prison en tant que {0.user}'.format(client))
 
 
 async def change_status():
     await client.wait_until_ready()
-    #Définition de la liste de statuts
+    # Définition de la liste de statuts
     statuses = [
         'Surveille la prison', 'Observe...', 'Fait le maton',
         'Récolte les pots de vins', 'Astique sa matraque',
@@ -28,7 +29,7 @@ async def change_status():
     ]
 
     while not client.is_closed():
-        #Choix aléatoire du statut
+        # Choix aléatoire du statut
         status = random.choice(statuses)
 
         await client.change_presence(activity=discord.Game(name=status))
@@ -36,110 +37,147 @@ async def change_status():
         await asyncio.sleep(300)
 
 
-@client.command()
 @commands.has_permissions(manage_roles=True)
 async def create_role(ctx):
 
     guild = ctx.guild
-    #Définition du nom du role
+    # Définition du nom du role
     name = 'Prisonnier'
-    #Création du role
+    # Création du role
     role = await guild.create_role(name=name)
     role.hoist = True
-    #Envoi de la confirmation de création du role
+    # Envoi de la confirmation de création du role
     await ctx.send(f'Role `{name}` has been created')
 
 
-@client.command()
-async def roles(ctx):
-    #Renvoie la liste des roles du serveur
+async def roles(ctx):  # Renvoie la liste des roles du serveur
     return ([(r.name) for r in ctx.guild.roles])
 
 
-@client.command()
-async def roles_membre(member):
-    #Renvoie la liste des roles du membre
+async def roles_membre(member):  # Renvoie la liste des id des roles du membre
     return ([(r.id) for r in member.roles])
 
-
-@client.command()
+# Renvoie la liste des noms des roles du membre
 async def noms_roles_membre(member):
-    #Renvoie la liste des noms des roles du membre
     return ([(r.name) for r in member.roles])
 
-
-@client.command()
-async def lst_categories(ctx):
-    #Renvoie la liste des catégories du serveur
+async def lst_categories(ctx):  # Renvoie la liste des catégories du serveur
     return ([(r.name) for r in ctx.guild.categories])
 
 
-@client.command()
+# Renvoie la liste des channel vocaux du serveur
 async def lst_voice_channels(ctx):
-    #Renvoie la liste des channel vocaux du serveur
     return ([(r.name) for r in ctx.guild.voice_channels])
 
 
-@client.command()
-async def update_role(ctx, name):
+async def update_role(ctx, name):  # Mets à jour le role passé en paramètre
     role = discord.utils.get(ctx.guild.roles, name=name)
     await role.edit(speak=False)
     role.hoist = True
     return None
 
-
-@client.command()
-async def prison(ctx, member: discord.Member, temps=5.0):
-
-    await update_role(ctx, '@everyone') 
-
-    list_role = await roles(ctx) #Liste des roles du serveur
-
-    if 'Prisonnier' not in list_role:
-        await create_role(ctx)#Créer un role 'Prisonnier'
-    await update_role(ctx, 'Prisonnier')
-
-    nom_role_membre = await noms_roles_membre(member) #Sauvegarde des roles du membre
-
-    #Bloc de suppression de tous les roles du membre
-    for i in nom_role_membre:
+async def suppr_roles(member,lst_roles):
+    for i in lst_roles:  # Bloc de suppression de tous les roles du membre
         role = discord.utils.get(member.guild.roles, name=f"{i}")
         if str(role) != "@everyone":
             await member.remove_roles(role)
 
-    role = discord.utils.get(member.guild.roles, name="Prisonnier")
-    await member.add_roles(role) #Ajout du role Prisonnier au membre
+@client.command()
+async def music(ctx,member: discord.Member):
+    await member.voice.channel.connect()
 
-    lst_cate = await lst_categories(ctx) #Liste des catégories
-    if 'Prison' not in lst_cate:
+
+@client.command()
+async def prison(ctx, member: discord.Member, temps=60):
+    
+    """
+    Attribution des valeurs des variables
+    """
+    max_temps = 600  # Temps de prison max
+    list_role = await roles(ctx)  # Liste des roles du serveur
+    # Sauvegarde des roles du membre
+    nom_role_membre = await noms_roles_membre(member)
+    try :
+        channel_origin = member.voice.channel.name  # Salon vocal du membre
+    except:
+        channel_origin = None # Le membre n'est pas connecté
+
+    """
+    Pré-réquis
+    """    
+    await update_role(ctx, '@everyone')
+
+    if 'Prisonnier' not in list_role:
+        await create_role(ctx)  # Créer un role 'Prisonnier'
+    await update_role(ctx, 'Prisonnier')
+
+    """
+    Conditions à vérifier
+    """
+
+    if temps > max_temps:
+        return await ctx.send(f'Oh là, calme toi Cowboy, laisse lui sa liberté d\'expression ! {temps} > {max_temps}')
+
+    if 'Prisonnier' in nom_role_membre:
+        return await ctx.send(f'Mais c\'est du harcèlement ! Laisse le `{member.name}` est déjà prisonnier !')
+
+    """
+    Début des actions
+    """
+    await suppr_roles(member,nom_role_membre)
+
+    role = discord.utils.get(member.guild.roles, name="Prisonnier")
+    await member.add_roles(role)  # Ajout du role Prisonnier au membre
+
+    # Création de la catégorie Prison
+    if 'Prison' not in await lst_categories(ctx):
         await ctx.guild.create_category('Prison')
 
-    lst_voice_channel = await lst_voice_channels(ctx) # Liste des salons vocaux
-
-    if 'Cellule' not in lst_voice_channel: 
-        category = discord.utils.get(ctx.guild.categories, name='Prison') 
+    # Création du salon vocal Cellule
+    if 'Cellule' not in await lst_voice_channels(ctx):
+        category = discord.utils.get(ctx.guild.categories, name='Prison')
         await ctx.guild.create_voice_channel('Cellule', category=category)
 
-    await member.edit(mute=True)  #Permet de mute l'utilisateur
-    await member.move_to(discord.utils.get(ctx.guild.voice_channels,name='Cellule'))  #Déplace l'utilisateur vers la cellue
+    """
+    Connection vocale du Bot avec soundboard prote prison
+    """
+    if not channel_origin == None :
+        await music(ctx,member)
+    
 
-    await asyncio.sleep(temps * 60) #Timer
+    # Déplace l'utilisateur vers la cellue
+    if not channel_origin == None :
+        await member.edit(mute=True)  # Mute l'utilisateur
+        await member.move_to(discord.utils.get(ctx.guild.voice_channels, name='Cellule'))
+    
+    await asyncio.sleep(temps)  # Temps d'emprisonnement
 
-    await member.edit(mute=False)  #Permet d'unmute l'utilisateur
 
+    """
+    Libération 
+    """
+    
+    await member.remove_roles(discord.utils.get(member.guild.roles, name="Prisonnier")) #Supprime le role Prisonnier
+    # Ecrit un message de confirmation de libération
+    await ctx.send(f"{member.name} a été libéré !")
 
-    #Bloc de rRéattribution des roles
-    for i in nom_role_membre: 
+    # Renvoie le membre à son salon vocal d'origine
+    if not channel_origin == None :
+        await member.edit(mute=False)  # Unmute l'utilisateur
+        await member.move_to(discord.utils.get(ctx.guild.voice_channels, name=channel_origin))
+
+    for i in nom_role_membre:  # Bloc de réattribution des roles
         role = discord.utils.get(member.guild.roles, name=f"{i}")
         if str(role) != "@everyone":
             await member.add_roles(role)
+    
 
 
-#Execute en boucle la fonction change_status()
+# Execute en boucle la fonction change_status()
 client.loop.create_task(change_status())
 
-#Script pour maintenir le bot en vie
+# Script pour maintenir le bot en vie
 keep_alive()
 
-#Permet de lancer le bot
+# Permet de lancer le bot
 client.run(os.getenv('TOKEN'))
