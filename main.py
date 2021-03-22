4bafe7bcaae1915e2c5a6dde392e893a03fdcd27
Théo <tete05092002@gmail.com>
2021-03-22 16:17:27 +0000

    Reformatage et ajout de fonctionnalités

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import discord
 import os
 import random
 import asyncio
-import time
+import nacl
 from discord.ext import commands
 from keep_alive import keep_alive
 
@@ -13,7 +13,7 @@ client = commands.Bot(command_prefix='!')
 async def on_ready():
     await client.change_presence(activity=discord.Game(
         name='Prend son service...'))
-    print('We have logged in as {0.user}'.format(client))
+    print('Nous sommes arrivé à la prison en tant que {0.user}'.format(client))
 
 
 async def change_status():
@@ -37,7 +37,6 @@ async def change_status():
         await asyncio.sleep(300)
 
 
-@client.command()
 @commands.has_permissions(manage_roles=True)
 async def create_role(ctx):
 
@@ -51,57 +50,61 @@ async def create_role(ctx):
     await ctx.send(f'Role `{name}` has been created')
 
 
-@client.command()
 async def roles(ctx):  # Renvoie la liste des roles du serveur
     return ([(r.name) for r in ctx.guild.roles])
 
 
-@client.command()
 async def roles_membre(member):  # Renvoie la liste des id des roles du membre
     return ([(r.id) for r in member.roles])
 
-
-@client.command()
 # Renvoie la liste des noms des roles du membre
 async def noms_roles_membre(member):
     return ([(r.name) for r in member.roles])
 
-
-@client.command()
 async def lst_categories(ctx):  # Renvoie la liste des catégories du serveur
     return ([(r.name) for r in ctx.guild.categories])
 
 
-@client.command()
 # Renvoie la liste des channel vocaux du serveur
 async def lst_voice_channels(ctx):
     return ([(r.name) for r in ctx.guild.voice_channels])
 
 
-@client.command()
 async def update_role(ctx, name):  # Mets à jour le role passé en paramètre
     role = discord.utils.get(ctx.guild.roles, name=name)
     await role.edit(speak=False)
     role.hoist = True
     return None
 
+async def suppr_roles(member,lst_roles):
+    for i in lst_roles:  # Bloc de suppression de tous les roles du membre
+        role = discord.utils.get(member.guild.roles, name=f"{i}")
+        if str(role) != "@everyone":
+            await member.remove_roles(role)
+
+@client.command()
+async def music(ctx,member: discord.Member):
+    await member.voice.channel.connect()
+
 
 @client.command()
 async def prison(ctx, member: discord.Member, temps=60):
-
-    try :
-        channel_origin = member.voice.channel.name  # Salon vocal du membre
-    except:
-        channel_origin= None
     
+    """
+    Attribution des valeurs des variables
+    """
     max_temps = 600  # Temps de prison max
     list_role = await roles(ctx)  # Liste des roles du serveur
     # Sauvegarde des roles du membre
     nom_role_membre = await noms_roles_membre(member)
+    try :
+        channel_origin = member.voice.channel.name  # Salon vocal du membre
+    except:
+        channel_origin = None # Le membre n'est pas connecté
 
     """
     Pré-réquis
-    """
+    """    
     await update_role(ctx, '@everyone')
 
     if 'Prisonnier' not in list_role:
@@ -121,13 +124,9 @@ async def prison(ctx, member: discord.Member, temps=60):
     """
     Début des actions
     """
-    for i in nom_role_membre:  # Bloc de suppression de tous les roles du membre
-        role = discord.utils.get(member.guild.roles, name=f"{i}")
-        if str(role) != "@everyone":
-            await member.remove_roles(role)
+    await suppr_roles(member,nom_role_membre)
 
     role = discord.utils.get(member.guild.roles, name="Prisonnier")
-
     await member.add_roles(role)  # Ajout du role Prisonnier au membre
 
     # Création de la catégorie Prison
@@ -139,6 +138,11 @@ async def prison(ctx, member: discord.Member, temps=60):
         category = discord.utils.get(ctx.guild.categories, name='Prison')
         await ctx.guild.create_voice_channel('Cellule', category=category)
 
+    """
+    Connection vocale du Bot avec soundboard prote prison
+    """
+    if not channel_origin == None :
+        await music(ctx,member)
     
 
     # Déplace l'utilisateur vers la cellue
