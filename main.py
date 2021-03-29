9207ed1eb580dd557cd2378c3dd4e8170d8b0a29
Théo <tete05092002@gmail.com>
2021-03-29 09:42:42 +0000

    Création du salon archiveprison avec réponses du bot

--- main.py
+++ main.py
@@ -60,6 +60,7 @@ async def roles_membre(member):  # Renvoie la liste des id des roles du membre
 async def noms_roles_membre(member):
     return ([(r.name) for r in member.roles])
 
+
 async def lst_categories(ctx):  # Renvoie la liste des catégories du serveur
     return ([(r.name) for r in ctx.guild.categories])
 
@@ -69,26 +70,28 @@ async def lst_voice_channels(ctx):
     return ([(r.name) for r in ctx.guild.voice_channels])
 
 
+# Renvoie la liste des channel textuels du serveur
+async def lst_text_channels(ctx):
+    return ([(r.name) for r in ctx.guild.text_channels])
+
+
 async def update_role(ctx, name):  # Mets à jour le role passé en paramètre
     role = discord.utils.get(ctx.guild.roles, name=name)
     await role.edit(speak=False)
     role.hoist = True
     return None
 
-async def suppr_roles(member,lst_roles):
+
+async def suppr_roles(member, lst_roles):
     for i in lst_roles:  # Bloc de suppression de tous les roles du membre
         role = discord.utils.get(member.guild.roles, name=f"{i}")
         if str(role) != "@everyone":
             await member.remove_roles(role)
 
-@client.command()
-async def music(ctx,member: discord.Member):
-    await member.voice.channel.connect()
-
 
 @client.command()
 async def prison(ctx, member: discord.Member, temps=60):
-    
+
     """
     Attribution des valeurs des variables
     """
@@ -96,72 +99,76 @@ async def prison(ctx, member: discord.Member, temps=60):
     list_role = await roles(ctx)  # Liste des roles du serveur
     # Sauvegarde des roles du membre
     nom_role_membre = await noms_roles_membre(member)
-    try :
+    try:
         channel_origin = member.voice.channel.name  # Salon vocal du membre
     except:
-        channel_origin = None # Le membre n'est pas connecté
+        channel_origin = None  # Le membre n'est pas connecté
 
     """
     Pré-réquis
-    """    
+    """
     await update_role(ctx, '@everyone')
 
     if 'Prisonnier' not in list_role:
         await create_role(ctx)  # Créer un role 'Prisonnier'
     await update_role(ctx, 'Prisonnier')
 
+    # Création de la catégorie Prison
+    if 'Prison' not in await lst_categories(ctx):
+        await ctx.guild.create_category('Prison')
+
+    category = discord.utils.get(ctx.guild.categories, name='Prison')
+
+    # Création du salon textuel archiveprison
+    if 'archiveprison' not in await lst_text_channels(ctx):
+        await ctx.guild.create_text_channel('ArchivePrison', category=category)
+
+    # Création du salon vocal Cellule
+    if 'Cellule' not in await lst_voice_channels(ctx):
+        await ctx.guild.create_voice_channel('Cellule', category=category)
+
+    archiveprison = discord.utils.get(
+        ctx.guild.text_channels, name='archiveprison')
+
     """
     Conditions à vérifier
     """
 
     if temps > max_temps:
-        return await ctx.send(f'Oh là, calme toi Cowboy, laisse lui sa liberté d\'expression ! {temps} > {max_temps}')
+        return await archiveprison.send(f'Oh là, calme toi Cowboy, laisse lui sa liberté d\'expression ! {temps} > {max_temps}')
 
     if 'Prisonnier' in nom_role_membre:
-        return await ctx.send(f'Mais c\'est du harcèlement ! Laisse le `{member.name}` est déjà prisonnier !')
+        return await archiveprison.send(f'Mais c\'est du harcèlement ! Laisse le `{member.name}` est déjà prisonnier !')
+    if ctx.message.author.top_role < member.top_role:
+        return await archiveprison.send('Oh là, tu essayes d\'emprisonner qui? Respecte ton supérieur !')
 
     """
     Début des actions
     """
-    await suppr_roles(member,nom_role_membre)
+    await suppr_roles(member, nom_role_membre)
 
     role = discord.utils.get(member.guild.roles, name="Prisonnier")
     await member.add_roles(role)  # Ajout du role Prisonnier au membre
 
-    # Création de la catégorie Prison
-    if 'Prison' not in await lst_categories(ctx):
-        await ctx.guild.create_category('Prison')
-
-    # Création du salon vocal Cellule
-    if 'Cellule' not in await lst_voice_channels(ctx):
-        category = discord.utils.get(ctx.guild.categories, name='Prison')
-        await ctx.guild.create_voice_channel('Cellule', category=category)
-
-    """
-    Connection vocale du Bot avec soundboard prote prison
-    """
-    #if not channel_origin == None :
-        #await music(ctx,member)
-    
-
     # Déplace l'utilisateur vers la cellue
-    if not channel_origin == None :
+    if not channel_origin == None:
         await member.edit(mute=True)  # Mute l'utilisateur
         await member.move_to(discord.utils.get(ctx.guild.voice_channels, name='Cellule'))
-    
-    await asyncio.sleep(temps)  # Temps d'emprisonnement
 
+    await asyncio.sleep(temps)  # Temps d'emprisonnement
 
     """
     Libération 
     """
+
+    # Supprime le role Prisonnier
+    await member.remove_roles(discord.utils.get(member.guild.roles, name="Prisonnier"))
     
-    await member.remove_roles(discord.utils.get(member.guild.roles, name="Prisonnier")) #Supprime le role Prisonnier
     # Ecrit un message de confirmation de libération
-    await ctx.send(f"{member.name} a été libéré !")
+    await archiveprison.send(f"{member.name} a été libéré !")
 
     # Renvoie le membre à son salon vocal d'origine
-    if not channel_origin == None :
+    if not channel_origin == None:
         await member.edit(mute=False)  # Unmute l'utilisateur
         await member.move_to(discord.utils.get(ctx.guild.voice_channels, name=channel_origin))
 
@@ -169,7 +176,6 @@ async def prison(ctx, member: discord.Member, temps=60):
         role = discord.utils.get(member.guild.roles, name=f"{i}")
         if str(role) != "@everyone":
             await member.add_roles(role)
-    
 
 
 # Execute en boucle la fonction change_status()
